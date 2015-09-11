d93a08d3d241d1ae493f4c332ec53d1128aed065
Kyoril <r.klimonow@chaoscore.net>
2015-09-11 22:07:53 +0200

    Added missing table to sql script

--- data/sql/wowpp_realm_struct.sql
+++ data/sql/wowpp_realm_struct.sql
@@ -72,6 +72,16 @@ CREATE TABLE `character_items` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
+DROP TABLE IF EXISTS `character_social`;
+
+CREATE TABLE `character_social` (
+  `guid_1` bigint(20) unsigned NOT NULL,
+  `guid_2` bigint(20) unsigned NOT NULL,
+  `flags` tinyint(3) unsigned NOT NULL,
+  `note` varchar(48) COLLATE latin1_german1_ci DEFAULT NULL,
+  PRIMARY KEY (`guid_1`,`guid_2`,`flags`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
