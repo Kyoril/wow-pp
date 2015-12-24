7c65a5a8e32905524da97b279595c5943d02396d
Kyoril <r.klimonow@chaoscore.net>
2015-12-24 15:42:21 +0100

    GCC fix

--- shared/proto_data/project_saver.h
+++ shared/proto_data/project_saver.h
@@ -63,14 +63,14 @@ namespace wowpp
 
 				template<class T>
 				static bool saveManagerToFile(
-					const String &fileName,
+					const String &filename,
 					const String &name,
 					const T &manager)
 				{
-					std::ofstream file(fileName.c_str(), std::ios::out | std::ios::binary);
+					std::ofstream file(filename.c_str(), std::ios::out | std::ios::binary);
 					if (!file)
 					{
-						ELOG("Could not save file '" << fileName << "'");
+						ELOG("Could not save file '" << filename << "'");
 						return false;
 					}
 
