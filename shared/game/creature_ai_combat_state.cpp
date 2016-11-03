2f27707d3db52b69964097d0ca94fdec7d557191
Kyoril <r.klimonow@chaoscore.net>
2016-11-03 20:18:14 +0100

    Fixed compile error

--- shared/game/creature_ai_combat_state.cpp
+++ shared/game/creature_ai_combat_state.cpp
@@ -61,7 +61,7 @@ namespace wowpp
 	CreatureAICombatState::~CreatureAICombatState()
 	{
 		// Disconnect all connected slots
-		m_nextActionCountdown.ended.clear();
+		m_nextActionCountdown.ended.disconnect_all_slots();
 	}
 
 	void CreatureAICombatState::onEnter()
@@ -363,7 +363,7 @@ namespace wowpp
 	void CreatureAICombatState::onLeave()
 	{
 		// Reset all events here to prevent them being fired in another ai state
-		m_nextActionCountdown.ended.clear();
+		m_nextActionCountdown.ended.disconnect_all_slots();
 
 		m_nextActionCountdown.cancel();
 		m_onThreatened.disconnect();
